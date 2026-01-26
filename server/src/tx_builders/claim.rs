87951ed88aca42ea51e9fc4140d0ccb8cee8d853
a-osiecki <a.osiecki@mi.unc.edu.ar>
2026-01-26 14:04:31 -0300

    update claim, badge, and merge modules

--- server/src/tx_builders/claim.rs
+++ server/src/tx_builders/claim.rs
@@ -1,13 +1,11 @@
 use axum::Json;
 use log::info;
-use tx3_sdk::trp::TxEnvelope;
-
 use crate::tx_builders::{
     protocol::{ClaimParams, PROTOCOL},
-    utils::handle_tx_result,
+    utils::{handle_tx_result, TxHandlerResult},
 };
 
-pub async fn claim(Json(req): Json<ClaimParams>) -> Json<Result<TxEnvelope, String>> {
+pub async fn claim(Json(req): Json<ClaimParams>) -> TxHandlerResult {
     info!("Received claim bounty request: {:?}", req);
 
     handle_tx_result(PROTOCOL.claim_tx(req).await).await
--- server/src/tx_builders/merge.rs
+++ server/src/tx_builders/merge.rs
@@ -1,9 +1,10 @@
 use axum::Json;
-use tx3_sdk::trp::TxEnvelope;
+use crate::tx_builders::{
+    protocol::{MergeParams, PROTOCOL},
+    utils::{handle_tx_result, TxHandlerResult},
+};
 
-use crate::tx_builders::{protocol::{PROTOCOL, MergeParams}, utils::handle_tx_result};
-
-pub async fn merge(Json(req): Json<MergeParams>) -> Json<Result<TxEnvelope, String>> {
+pub async fn merge(Json(req): Json<MergeParams>) -> TxHandlerResult {
     log::info!("Received merge request: {:?}", req);
 
     handle_tx_result(PROTOCOL.merge_tx(req).await).await
